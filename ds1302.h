338f35a600b2c691658d105b25cd0c829e714fca
zyp1647519079 <55043250+zyp1647519079@users.noreply.github.com>
2019-09-24 07:59:32 +0800

    Update ds1302.h

--- ds1302.h
+++ ds1302.h
@@ -1,22 +1,15 @@
-#ifndef _ds1302_1022
-#define _ds1302_1022
+#ifndef_ds1302_1022
+#defing_ds1302_1022
 #include "stdio.h"
-//******ds1302与at89s52引脚连接*****
-
 sbit T_RST=P3^7; //DS1302接线
 sbit T_CLK=P3^5; // DS1302接线 
 sbit T_IO=P3^6; // DS1302接线 
 sbit ACC0=ACC^0;
 sbit ACC7=ACC^7;
-
-/***********************延时程序=a*2us**************************************/
 void delay(unsigned int a)
 {
 while(a--); 
 }
-/****************************************************
-// DS1302?
-/******************DS1302：写入操作(上升沿)*********************/ 
 void write_byte(unsigned char ucode)
 {
 unsigned char i;
@@ -30,8 +23,6 @@ T_CLK=1;
 ACC=ACC>>1;
 }
 }
-
-/******************DS1302：读取操作（下降沿）*****************/
 unsigned char read_byte(void)
 {
 unsigned char i;
@@ -47,8 +38,6 @@ ACC7 = T_IO;
 return(ACC);
 
 }
-
-/******************DS1302:写入数据（先送地址，再写数据）***************************/ 
 void write_1302(unsigned char addr,unsigned char ucode)
 {
 T_RST=0; //停止工作
@@ -60,8 +49,6 @@ write_byte(ucode);
 T_RST=0;
 T_CLK=1;
 }
-
-/******************DS1302:读取数据（先送地址，再读数据）**************************/
 unsigned char read_1302(unsigned char addr)
 {
 unsigned char ucode;
